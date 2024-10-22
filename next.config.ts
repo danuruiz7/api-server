16d23f1825fef764332691221e1dd396bf8e7f7a
Daniel Ruiz <129541275+danuruiz7@users.noreply.github.com>
2024-10-22 21:06:56 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/next-app', // Configura la base path
 };
 
 export default nextConfig;
